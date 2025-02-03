236eeadd04c4fb27725c7741c601c996d2559511
Raj Kumar Jha <60534304+Raj-k-Jha@users.noreply.github.com>
2025-02-03 17:41:56 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hi, We will decide your band name, Just answer below questions?")
+City = input("what is your city name?\n")
+Animal = input("Okay, Now tell your favourite animal? \n")
+print ("Your band name is " + City +" "+ Animal)
